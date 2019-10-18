74284e85cc55bf722474f08f8b0460c6348c8c02
Max Mitchell <rat2043@gmail.com>
2019-10-18 12:33:05 -0700

    added temp code to main

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("welcome to space Quest!")
+	fmt.Println("What is your name?")
+	fmt.Println("John")
+	fmt.Println("Hello John, do you want me to pick a planet for you?(Y/N)")
+	fmt.Println("N")
+	fmt.Println("What planet do you want to go to?")
+	fmt.Println("Mars")
+	fmt.Println("Welcome to Mars!")
+	fmt.Println("It is a Red planet and is cold")
+}
