a03d76fbcd43009891214b713d6d16070a234598
MaxTheMitchell <rat2043@gmail.com>
2019-10-20 15:10:03 -0700

    added json data reaading

--- main.go
+++ main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"os"
+	"strconv"
 )
+
+type PlanetarySystem struct {
+	Name    string   `json:"name"`
+	Planets []Planet `json:"planets"`
+}
+
+type Planet struct {
+	Name string `json:"name"`
+	Info string `json:"description"`
+}
 
 func main() {
 
+	file, err := os.Open("planetarySystem.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	byteVaue, _ := ioutil.ReadAll(file)
+
+	var planetSyst PlanetarySystem
+
+	json.Unmarshal(byteVaue, &planetSyst)
+
 	var input string
-	planets := make(map[int][]string)
-	planets[0] = append(planets[0], "Mars")
-	planets[0] = append(planets[0], "The red one")
-	planets[1] = append(planets[1], "pluto")
-	planets[1] = append(planets[1], "the fake one")
 
 	fmt.Println("welcome to space Quest!")
-	fmt.Println("we are in the solar system and it has 8 planets!")
+	fmt.Println("we are in the " + planetSyst.Name + " planetary system, it has " + strconv.Itoa(len(planetSyst.Planets)) + " planets!")
 	fmt.Println("What is your name?")
 	fmt.Scanln(&input)
-	goToPlanet("Hello "+input+", do you want me to pick a planet for you?(Y/N)", planets)
+	goToPlanet("Hello "+input+", do you want me to pick a planet for you?(Y/N)", planetSyst)
 
 }
 
@@ -28,23 +51,24 @@ func tellabout(planet string, info string) {
 
 }
 
-func goToPlanet(intro string, planets map[int][]string) {
+func goToPlanet(intro string, planetSyst PlanetarySystem) {
 	fmt.Println(intro)
-	var yn string
-	fmt.Scanln(&yn)
-	if yn == "N" || yn == "n" {
+	var input string
+	fmt.Scanln(&input)
+	if input == "N" || input == "n" {
 		fmt.Println("What planet do you want to go to?")
-		fmt.Scanln(&yn)
-		for _, list := range planets {
-			if list[0] == yn {
-				tellabout(list[0], list[1])
+		fmt.Scanln(&input)
+		// var planet Planet
+		for _, planet := range planetSyst.Planets {
+			if planet.Name == input {
+				tellabout(planet.Name, planet.Info)
 			}
 		}
-	} else if yn == "Y" || yn == "y" {
-		randIndex := rand.Intn(2)
-		tellabout(planets[randIndex][0], planets[randIndex][1])
+	} else if input == "Y" || input == "y" {
+		planet := planetSyst.Planets[rand.Intn(len(planetSyst.Planets))]
+		tellabout(planet.Name, planet.Info)
 
 	} else {
-		goToPlanet("Invalid input, enter (Y/N)", planets)
+		goToPlanet("Invalid input, enter (Y/N)", planetSyst)
 	}
 }
