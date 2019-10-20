bf00b0bce1350ecc777455bd76140202c07928cd
MaxTheMitchell <rat2043@gmail.com>
2019-10-20 12:50:28 -0700

    Added reprompt for invalid input

--- main.go
+++ main.go
@@ -15,28 +15,36 @@ func main() {
 	planets[1] = append(planets[1], "the fake one")
 
 	fmt.Println("welcome to space Quest!")
+	fmt.Println("we are in the solar system and it has 8 planets!")
 	fmt.Println("What is your name?")
 	fmt.Scanln(&input)
-	fmt.Println("Hello " + input + ", do you want me to pick a planet for you?(Y/N)")
-	fmt.Scanln(&input)
+	goToPlanet("Hello "+input+", do you want me to pick a planet for you?(Y/N)", planets)
 
-	if input == "N" {
-		fmt.Println("What planet do you want to go to?")
-		fmt.Scanln(&input)
-		for _, list := range planets {
-			if list[0] == input {
-				tellabout(list[0], list[1])
-			}
-		}
-	} else {
-		randIndex := rand.Intn(2)
-		tellabout(planets[randIndex][0], planets[randIndex][1])
-
-	}
 }
 
 func tellabout(planet string, info string) {
 	fmt.Println("Let me tell you about " + planet)
 	fmt.Println(info)
 
+}
+
+func goToPlanet(intro string, planets map[int][]string) {
+	fmt.Println(intro)
+	var yn string
+	fmt.Scanln(&yn)
+	if yn == "N" || yn == "n" {
+		fmt.Println("What planet do you want to go to?")
+		fmt.Scanln(&yn)
+		for _, list := range planets {
+			if list[0] == yn {
+				tellabout(list[0], list[1])
+			}
+		}
+	} else if yn == "Y" || yn == "y" {
+		randIndex := rand.Intn(2)
+		tellabout(planets[randIndex][0], planets[randIndex][1])
+
+	} else {
+		goToPlanet("Invalid input, enter (Y/N)", planets)
+	}
 }
