dd842ef07ebfdf6a415cdcb2b96971d5349ea9a9
MaxTheMitchell <rat2043@gmail.com>
2019-10-19 23:11:07 -0700

    added map and user input

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
+	planets := make(map[string]string)
+	planets["Mars"] = "The Red one"
+	planets["pluto"] = "The not real one"
+
 	fmt.Println("welcome to space Quest!")
 	fmt.Println("What is your name?")
 	fmt.Println("John")
 	fmt.Println("Hello John, do you want me to pick a planet for you?(Y/N)")
-	fmt.Println("N")
-	fmt.Println("What planet do you want to go to?")
-	fmt.Println("Mars")
-	fmt.Println("Welcome to Mars!")
-	fmt.Println("It is a Red planet and is cold")
+
+	var input string
+	fmt.Scanln(&input)
+
+	if input == "N" {
+		fmt.Println("What planet do you want to go to?")
+		fmt.Scanln(&input)
+		for planet, info := range planets {
+			if planet == input {
+				fmt.Println(info)
+			}
+		}
+	}
 }
